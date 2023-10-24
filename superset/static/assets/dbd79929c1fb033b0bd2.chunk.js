9f372693e308a08af98851241eb97356cede55c9
unknown <zhugy@kongsunfin.com>
2023-10-24 17:40:18 +0800

    npm run build in original branch

---
+++ superset/static/assets/dbd79929c1fb033b0bd2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunksuperset=globalThis.webpackChunksuperset||[]).push([[468],{82343:(s,e,r)=>{r.r(e),r.d(e,{default:()=>l}),r(67294);var u=r(5977),a=r(37936),t=r(11965);const l=()=>{const{idOrSlug:s}=(0,u.UO)();return(0,t.tZ)(a.DashboardPage,{idOrSlug:s})}}}]);
+//# sourceMappingURL=dbd79929c1fb033b0bd2.chunk.js.map
